8b09d280eb46f90711dba27a48c0f39ad82667d7
Gustavo Novais <gustavonovais.dev@gmail.com>
2024-10-08 08:12:47 -0300

    [FEAT] Ex03 array

--- src/ex_array/Ex03.java
+++ src/ex_array/Ex03.java
@@ -5,38 +5,63 @@ import java.util.Scanner;
 
 public class Ex03 {
 
-    public static void main(String[] args) {
+    //Fazer um programa para ler nome, idade e altura de N pessoas, conforme exemplo. Depois, mostrar na
+    //tela a altura média das pessoas, e mostrar também a porcentagem de pessoas com menos de 16 anos,
+    //bem como os nomes dessas pessoas caso houver.
+
+    public static void main(String[] args){
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n, maioridade, posicaomaior;
+        System.out.println("Quantidade de pessoas:");
 
-        System.out.print("Quantas pessoas voce vai digitar? ");
-        n = sc.nextInt();
+        int N = sc.nextInt();
 
-        String[] nomes = new String[n];
-        int[] idades = new int[n];
+        String[] nome = new String[N];
+        Integer[] idade = new Integer[N];
+        Double[] altura = new Double[N];
 
-        for (int i=0; i<n; i++) {
-            System.out.printf("Dados da %da pessoa:\n", i + 1);
-            System.out.print("Nome: ");
-            nomes[i] = sc.next();
-            System.out.print("Idade: ");
-            idades[i] = sc.nextInt();
+        //coletando informações
+        for(int i=0; i < N; i++){
+            System.out.println("Escreva o nome:");
+            nome[i] = sc.next();
+
+            System.out.println("Escreva a idade:");
+            idade[i] = sc.nextInt();
+
+            System.out.println("Escreva a altura:");
+            altura[i] = sc.nextDouble();
         }
 
-        maioridade = idades[0];
-        posicaomaior = 0;
+        //altura media
+        double media = 0;
+        for(int i=0; i < altura.length; i++){
+            media += altura[i];
+        }
+        System.out.printf("Media de altura: %.02f" , media/ altura.length);
+        System.out.println();
 
-        for (int i=1; i<n; i++) {
-            if (idades[i] > maioridade) {
-                maioridade = idades[i];
-                posicaomaior = i;
+        //porcentagem e nomes de pessoas com menos de 16 anos
+        int qtdMenores = 0;
+        int totalMenores = 0;
+        for(int i=0; i < N; i++){
+            if(idade[i] < 16){
+                qtdMenores++;
+            }
+
+            totalMenores += qtdMenores;
+        }
+
+        double percentualMenores = ((double)totalMenores / N) * 100.00;
+        System.out.printf("Porcentagem de menores de idade: %.02f" , percentualMenores);
+        System.out.println();
+
+        for(int i=0; i < N; i++){
+            if (idade[i] < 16) {
+                System.out.println("Lista dos nomes da galerinha menor de idade: " + nome[i]);
             }
         }
-
-        System.out.printf("PESSOA MAIS VELHA: %s\n", nomes[posicaomaior]);
 
         sc.close();
     }
