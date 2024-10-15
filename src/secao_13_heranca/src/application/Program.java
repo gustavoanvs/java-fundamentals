74839f806c403a0b4e9eabd91b878a01faa8b7cc
Gustavo Novais <gustavonovais.dev@gmail.com>
2024-10-14 22:18:09 -0300

    [FEAT] Upcasting

--- src/secao_13_heranca/src/application/Program.java
+++ src/secao_13_heranca/src/application/Program.java
@@ -1,8 +1,19 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
 
     public static void main(String[] args){
+
+        Account acc = new Account(01, "Gustavo", 0.0);
+        BusinessAccount bacc = new BusinessAccount(02, "Gabriel", 0.0, 1000.0);
+
+        // UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(03, "Guilherme", 0.0, 0.0);
 
     }
 
