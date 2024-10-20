92256c1e2d0f2df5eb7b4463ed8427b0ef0810a8
Gustavo Novais <gustavonovais.dev@gmail.com>
2024-10-20 15:38:36 -0300

    [FEAT] StackTrace

--- src/secao_excecoes/src/application/Program.java
+++ src/secao_excecoes/src/application/Program.java
@@ -7,10 +7,23 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args){
-
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
+        method1();
+
+        System.out.println("Fim da aplicação");
+
+    }
+
+    public static void method1(){
+        System.out.println("*** Começo do Method1 ***");
+        method2();
+        System.out.println("*** Fim do Method1 ***");
+    }
+
+    public static void method2(){
+        System.out.println("*** Começo do Method2 ***");
+        Scanner sc = new Scanner(System.in);
         try {
             System.out.println("Escreva alguns nomes na lista separado por espaço");
             String[] vect = sc.nextLine().split(" ");
@@ -18,14 +31,16 @@ public class Program {
             System.out.println("Qual posição da lista você esta afim de ver?");
             int position = sc.nextInt();
             System.out.println(vect[position]);
-        } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Posição Inválida");
-        } catch (InputMismatchException e){
-            System.out.println("Inserção errada, digite o numero da posição");
         }
-
-        System.out.println("Fim da aplicação");
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Posição Inválida");
+            e.printStackTrace();
+            sc.next();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Inserção errada, digit o numero da posição");
+        }
         sc.close();
+        System.out.println("*** Fim do Method2 ***");
     }
-
 }
