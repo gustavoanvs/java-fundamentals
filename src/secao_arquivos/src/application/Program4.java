ff3ff7eb070dd5f26a30af7f43d7ddc222c61f14
Gustavo Novais <gustavonovais.dev@gmail.com>
2024-10-31 22:26:57 -0300

    [FEAT] File/Folder

---
+++ src/secao_arquivos/src/application/Program4.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program4 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String srtPath = sc.nextLine(); //H:\Estudos\Java
+
+        File path = new File(srtPath);
+
+        File[] folders = path.listFiles(File::isDirectory); //somente dir ou pasta
+        File[] files = path.listFiles(File::isFile); //somente arquivos
+
+        System.out.println("Folders:");
+        for (File folder : folders){
+            System.out.println(folder);
+        }
+
+        for (File file : files){
+            System.out.println(file);
+        }
+
+        boolean success = new File(srtPath + "\\suasbdir").mkdir(); //criar uma sub pasta chamada subdir ...
+        System.out.println("Dir created: " + success);
+
+        sc.close();
+
+    }
+
+}
